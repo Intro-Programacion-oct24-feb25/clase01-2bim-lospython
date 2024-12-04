7c36e60fd248dda0c632bd09a356fad40d6d40bb
Emilio Poma <ejpoma2@utpl.edu.ec>
2024-12-04 12:06:30 -0500

    Clase 01-2b

--- ManejoCicloFor/src/paquete04/Ejemplo015.java
+++ ManejoCicloFor/src/paquete04/Ejemplo015.java
@@ -6,6 +6,8 @@
 package paquete04;
 
 import paquete01.*;
+import java.util.Locale;
+import java.util.Scanner;
 
 /**
  *
@@ -17,12 +19,29 @@ public class Ejemplo015 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US); 
         
-        for (int i = 10; i >= 0; i--) {
-            System.out.printf("%d\n", i);
+        int factor_1 = 1;
+        int factor_2 = 1;
+        int limite;
+        int limite_tabla;
+        
+        System.out.println("Ingrese el numero de tablas");
+        limite = entrada.nextInt();
+        System.out.println("Ingrese el limite de las tablas");
+        limite_tabla = entrada.nextInt();
+        
+        while (factor_1 <= limite){
+            while (factor_2 <= limite_tabla){
+                System.out.println();
+            }
+            
+        }
+        
         }
         
         
     }
     
-}
+
