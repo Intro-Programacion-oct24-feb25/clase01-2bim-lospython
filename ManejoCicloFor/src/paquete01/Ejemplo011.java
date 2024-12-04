d42b757e8c49214ae12e81ecab0807fc987c7df0
Emilio Poma <ejpoma2@utpl.edu.ec>
2024-12-04 11:58:06 -0500

    Clase 01-2b

--- ManejoCicloFor/src/paquete01/Ejemplo011.java
+++ ManejoCicloFor/src/paquete01/Ejemplo011.java
@@ -15,15 +15,11 @@ public class Ejemplo011 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-<<<<<<< HEAD
+
         for (int i = 10; i >= 0; i--) {
             System.out.printf("Numeros enteros :\n %d\n", i);
     
-=======
-        for (int i = 20; i >= 0; i--) {
-            System.out.printf("%d\n", i); // Viva el fornai
->>>>>>> d23fdecb560acbe807c3df3acc134564743818a5
+
         }
         
         
