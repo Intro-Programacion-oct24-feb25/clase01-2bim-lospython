68d4c63579d97bfef248d10c39509eb824f4c153
Emilio Poma <ejpoma2@utpl.edu.ec>
2024-12-04 12:02:27 -0500

    Clase 01-2b

--- ManejoCicloFor/src/paquete04/Ejemplo015.java
+++ ManejoCicloFor/src/paquete04/Ejemplo015.java
@@ -1,13 +1,28 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package paquete04;
 
+import paquete01.*;
+
 /**
  *
- * @author utpl
+ * @author reroes
  */
 public class Ejemplo015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i = 10; i >= 0; i--) {
+            System.out.printf("%d\n", i);
+        }
+        
+        
+    }
     
 }
